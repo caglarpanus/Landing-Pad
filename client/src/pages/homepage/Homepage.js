df7aa75ba68a880749947555fd55037100af089e
unknown <jtc7537@gmail.com>
2018-04-25 20:16:38 -0400

    set up basic clientside routing

---
+++ client/src/pages/homepage/Homepage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Homepage extends React.Component {
+
+    constructor(){
+        super();
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return(
+            <div>This is the homepage!</div>
+        )
+    }
+
+}
+
+export default Homepage;
---
+++ client/src/pages/login/LoginPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Login extends React.Component {
+    
+    constructor(){
+        super();
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return(
+            <div>This is the login page!</div>
+        )
+    }
+}
+
+export default Login;
