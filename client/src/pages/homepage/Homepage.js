22c33c6ba1ad1a3ff453a9596ab83185c2cc8df6
Bettina Junghahn <bettinajunghahn@gmail.com>
2018-04-27 15:46:45 -0400

    background component

--- client/src/pages/homepage/Homepage.js
+++ client/src/pages/homepage/Homepage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './../../components/header/Header';
+import Background from './../../components/background/Background';
 
 class Homepage extends React.Component {
 
@@ -13,35 +14,35 @@ class Homepage extends React.Component {
     render(){
         return(
             <div className="container" id="full-body">
+                <Background backgroundImage="https://image.freepik.com/free-vector/city-background-design_1300-365.jpg">
 
-                <div className="row" id="top-line">
-                    <div className="text-center" id="options">
-                        <a className="navbar-brand activity" href="#">Activity</a>
-                        <a className="navbar-brand activity" href="#">Parking</a>
-                        <a className="navbar-brand activity" href="#">FAQs</a>
+                    <div className="row" id="top-line">
+                        <div className="text-center" id="options">
+                            <a className="navbar-brand activity" href="#">Activity</a>
+                            <a className="navbar-brand activity" href="#">Parking</a>
+                            <a className="navbar-brand activity" href="#">FAQs</a>
+                        </div>
+                        <i className="material-icons home-icon">settings_applications</i>
+                        <i className="material-icons home-icon">account_box</i>
                     </div>
-                    <i className="material-icons home-icon">settings_applications</i>
-                    <i className="material-icons home-icon">account_box</i>
-                </div>
-                <div className="row">
-                    <nav className="navbar navbar-light d-flex justify-content-center" id="home-nav">
-                        <a className="navbar-brand text-center" id="home-title" href="#">
-                        Landing Pad
-                        </a>
-                    </nav>
-
-                </div>
-                    <button type="button" className="btn btn-lg btn-outline-primary" id="park-button">Find Parking Near Me</button>
-                    <button type="button" className="btn btn-lg btn-outline-primary" id="rent-button">Upload A New Parking Spot</button>
-
-
-                <div className="row">
-                        <nav className="navbar navbar-light fixed-bottom justify-content-center" id="footer">
-                            <span id="footer-text">Ⓒ Copyright 2018</span>
+                    <div className="row">
+                        <nav className="navbar navbar-light d-flex justify-content-center" id="home-nav">
+                            <a className="navbar-brand text-center" id="home-title" href="#">
+                            Landing Pad
+                            </a>
                         </nav>
-                </div>
-                
-                
+
+                    </div>
+                        <button type="button" className="btn btn-lg btn-outline-primary" id="park-button">Find Parking Near Me</button>
+                        <button type="button" className="btn btn-lg btn-outline-primary" id="rent-button">Upload A New Parking Spot</button>
+
+
+                    <div className="row">
+                            <nav className="navbar navbar-light fixed-bottom justify-content-center" id="footer">
+                                <span id="footer-text">Ⓒ Copyright 2018</span>
+                            </nav>
+                    </div>
+                </Background>
             </div>
         )
     }
--- client/src/pages/login/LoginPage.js
+++ client/src/pages/login/LoginPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './../../components/header/Header';
+import Background from './../../components/background/Background';
 
 class Login extends React.Component {
     
@@ -13,21 +14,21 @@ class Login extends React.Component {
     render(){
         return(
             <div className="container" id="full-body">
-        
-                <div className="row">
+                <Background backgroundImage="https://image.freepik.com/free-vector/city-background-design_1300-365.jpg">
                     <Header/>
-                </div>
-                
-                <button type="button" className="btn btn-lg btn-outline-primary home-buttons">Sign In</button>
-                <button type="button" className="btn btn-lg btn-outline-primary home-buttons">Sign Up</button>
+                    <div className="row">
+                    </div>
+                        <button type="button" className="btn btn-lg btn-outline-primary home-buttons">Sign In</button>
+                        <button type="button" className="btn btn-lg btn-outline-primary home-buttons">Sign Up</button>
+                    
+                    
             
-        
-                <div className="row">
-                    <nav className="navbar navbar-light fixed-bottom justify-content-center" id="footer">
-                        <span id="footer-text">Ⓒ Copyright 2018</span>
-                    </nav>
-                </div>
-    
+                    <div className="row">
+                        <nav className="navbar navbar-light fixed-bottom justify-content-center" id="footer">
+                            <span id="footer-text">Ⓒ Copyright 2018</span>
+                        </nav>
+                    </div>
+                </Background>
             </div>
         )
     }
