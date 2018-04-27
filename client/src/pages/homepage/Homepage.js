d8a8257468e3740c383b63ef2eba42f32bffe94e
Bettina Junghahn <bettinajunghahn@gmail.com>
2018-04-27 17:13:04 -0400

    Header component and Homepage Page setup

--- client/src/pages/homepage/Homepage.js
+++ client/src/pages/homepage/Homepage.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Header from './../../components/header/Header';
 import Background from './../../components/background/Background';
 import Footer from './../../components/footer/Footer';
+import Title from './../../components/title/Title';
+import { Button } from 'reactstrap';
+
 
 import './Homepage.css';
 
@@ -18,34 +21,13 @@ class Homepage extends React.Component {
         return(
             <div className="container" id="full-body">
                 <Background backgroundImage="https://image.freepik.com/free-vector/city-background-design_1300-365.jpg">
-
-                    <div className="row" id="top-line">
-                        <div className="text-center" id="options">
-                            <a className="navbar-brand activity" href="#">Activity</a>
-                            <a className="navbar-brand activity" href="#">Parking</a>
-                            <a className="navbar-brand activity" href="#">FAQs</a>
-                        </div>
-                        <i className="material-icons home-icon">settings_applications</i>
-                        <i className="material-icons home-icon">account_box</i>
+                    <Header/>
+                    <Title/>
+                     <div className=" fixed-bottom park-btn-div">
+                        <Button outline color="primary" className="park-btn">Find Parking Near Me</Button>
+                        <Button outline color="primary" className="park-btn">Upload a New Parking Spot</Button>
                     </div>
-                    <div className="row">
-                        <nav className="navbar navbar-light d-flex justify-content-center" id="home-nav">
-                            <a className="navbar-brand text-center" id="home-title" href="#">
-                            Landing Pad
-                            </a>
-                        </nav>
-
-                    </div>
-                        <button type="button" className="btn btn-lg btn-outline-primary" id="park-button">Find Parking Near Me</button>
-                        <button type="button" className="btn btn-lg btn-outline-primary" id="rent-button">Upload A New Parking Spot</button>
-
-
-                    <div className="row">
-                            <nav className="navbar navbar-light fixed-bottom justify-content-center" id="footer">
-                                <span id="footer-text">Ⓒ Copyright 2018</span>
-                            </nav>
-                    </div>
-                    <Footer />
+                    <Footer/>
                 </Background>
             </div>
         )
