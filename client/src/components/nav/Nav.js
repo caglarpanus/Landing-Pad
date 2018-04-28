cbc6b033ffea9160483118fbc4f232d8ec198534
unknown <jtc7537@gmail.com>
2018-04-28 18:39:49 -0400

    asdf

--- client/src/components/nav/Nav.js
+++ client/src/components/nav/Nav.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import Button from 'material-ui/Button'
+import PropTypes from 'prop-types';
+import Button from 'material-ui/Button';
+import { withStyles } from 'material-ui/styles';
+import AppBar from 'material-ui/Toolbar';
+import Typograpy from 'material-ui/Typography';
+import IconButton from 'material-ui/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 class Nav extends React.Component {
 
