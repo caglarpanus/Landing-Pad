73c6c37d7c13a516d8242be5abc4d4dec818a5bb
caglarpanus <panuscaglar@gmail.com>
2018-05-02 15:01:29 -0400

    sign in takes you to homepage

--- client/src/pages/login/SignupPage.js
+++ client/src/pages/login/SignupPage.js
@@ -56,7 +56,7 @@ class Login extends React.Component {
     
         axios.post('/api/auth/signup', { username, password })
           .then((result) => {
-            this.props.history.push("/login")
+            this.props.history.push("/homepage")
           });
       }
     
