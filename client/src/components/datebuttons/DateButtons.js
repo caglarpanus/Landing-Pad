cc22bd48a3d7789ab4efdc54c44e9c987b48e263
unknown <jtc7537@gmail.com>
2018-05-02 23:18:15 -0400

    new space page

---
+++ client/src/components/datebuttons/DateButtons.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class DayButton extends React.Component {
+
+    constructor() {
+        super(props)
+    }
+
+    render() {
+
+        return(
+            <div>
+                <input 
+                    
+                />
+            </div>
+        )
+    }
+
+}
