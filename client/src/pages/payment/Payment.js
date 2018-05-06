0b104a4a6004c9e8cfb57a9d1bb3e057401caf0d
Bettina Junghahn <bettinajunghahn@gmail.com>
2018-05-06 17:12:14 -0400

    nada

--- client/src/pages/payment/Payment.js
+++ client/src/pages/payment/Payment.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Footer from './../../components/footer/Footer';
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, CardLink, Button, ListGroup, 
+    ListGroupItem } from 'reactstrap';
 
 import './Payment.css';
 
@@ -14,7 +17,15 @@ class Payment extends React.Component {
 
     render(){
         return(
-            <div>This is the payment page!</div>
+            <div className="container" id="solid-bckg">
+               <div className="grn-hdr"><Header/></div> 
+            
+            
+                <div className=" fixed-bottom new-btn-div text-center">
+                    <Button outline color="primary" id="directions">Checkout</Button>
+                </div>
+                <Footer/>
+            </div>
         )
     }
 }
