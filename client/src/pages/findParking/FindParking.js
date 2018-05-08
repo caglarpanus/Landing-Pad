04b719b6fd005e1a8e04337087ec8385f8cef7cc
Bettina Junghahn <bettinajunghahn@gmail.com>
2018-05-08 16:48:29 -0400

    find parking layout

--- client/src/pages/findParking/FindParking.js
+++ client/src/pages/findParking/FindParking.js
@@ -18,8 +18,6 @@ class FindParking extends React.Component {
         }
     }
 
-
-
     render(){
         return(
             <div>
@@ -27,9 +25,35 @@ class FindParking extends React.Component {
                     <div className="grn-hdr">
                         <Header/>
                     </div> 
+                    <button id='jeb' onClick={geocoder.getCoordFromAddress}>Geocoder</button>
+                    <button id='jeb2' onClick={geocoder.distanceMatrix}>Distance Matrix</button>
+
+                    <div className="row text-center" id="second-line">
+                        <div className="input-group mb-3">
+                            <input type="text" className="form-control" id="search-bar" placeholder="Search by Location" aria-label="Location Search" aria-describedby="basic-addon2" />
+                            <div className="input-group-append">
+                                <button className="btn btn-outline-primary" type="button" id="search-button">Search</button>
+                            </div>
+                        </div>
+                        <div className="btn-group" role="group" aria-label="Basic example">
+                            <Button type="button" className="btn btn-outline-primary home-buttons">Nearby</Button>
+                            <Button type="button" className="btn btn-outline-primary home-buttons">Recent</Button>
+                            <Button type="button" className="btn btn-outline-primary home-buttons">Favorites</Button>
+                        </div>
+                        </div>
+
+                        <div className="row">
+                        <div className="col-xs-12 justify-content-center" id="map-div">
+                            <img src="https://image.shutterstock.com/z/stock-photo-map-with-pins-markers-simple-flat-illustration-city-plan-with-streets-raster-version-633021710.jpg" alt="sample map" id="map" />
+                        </div>
+                        </div>
+
+                        <div className=" fixed-bottom new-btn-div text-center">
+                        <Button outline color="primary" className="new-btn">New Search</Button>
+                    </div>
+                    <Footer/>
                 </div>
-                <button id='jeb' onClick={geocoder.getCoordFromAddress}>Geocoder</button>
-                <button id='jeb2' onClick={geocoder.distanceMatrix}>Distance Matrix</button>
+                
             </div>
             
         
@@ -40,27 +64,5 @@ class FindParking extends React.Component {
 export default FindParking;
 
 
-const prevBody = `<div className="row text-center" id="second-line">
-<div className="input-group mb-3">
-    <input type="text" className="form-control" id="search-bar" placeholder="Search by Location" aria-label="Location Search" aria-describedby="basic-addon2" />
-    <div className="input-group-append">
-        <button className="btn btn-outline-primary" type="button" id="search-button">Search</button>
-    </div>
-</div>
-<div className="btn-group" role="group" aria-label="Basic example">
-    <button type="button" className="btn btn-outline-primary home-buttons">Nearby</button>
-    <button type="button" className="btn btn-outline-primary home-buttons">Recent</button>
-    <button type="button" className="btn btn-outline-primary home-buttons">Favorites</button>
-</div>
-</div>
-
-<div className="row">
-<div className="col-xs-12 justify-content-center" id="map-div">
-    <img src="https://image.shutterstock.com/z/stock-photo-map-with-pins-markers-simple-flat-illustration-city-plan-with-streets-raster-version-633021710.jpg" alt="sample map" id="map" />
-</div>
-</div>
-
-<div className=" fixed-bottom new-btn-div text-center">
-<Button outline color="primary" className="new-btn">New Search</Button>
-</div>
+const prevBody = `
 `
