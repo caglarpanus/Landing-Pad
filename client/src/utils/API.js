d1ad458637db917c5b798d3bc7c441396eb4514e
caglarpanus <panuscaglar@gmail.com>
2018-05-14 13:30:22 -0400

    receiving data from Parkwhiz

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -15,6 +15,6 @@ export default {
 
     searchParking: function(lat, lng){
         console.log(ParkWhizURL + lat + lng + "&start_time=2018-05-01T12:00&end_time=2019-12-01T20:00&returns=offstreet_bookable" + ParkWhizKEY);
-        return axios.get(ParkWhizURL + lat + lng + "&start_time=2018-05-01T12:00&end_time=2019-12-01T20:00&returns=offstreet_bookable" + ParkWhizKEY);
+        return axios.get(ParkWhizURL + lat +","+ lng + "&start_time=2018-05-01T12:00&end_time=2019-12-01T20:00&returns=offstreet_bookable" + ParkWhizKEY);
     }
 }
