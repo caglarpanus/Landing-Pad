34576f71079f7a22550def17dbb75a394f05a5f3
unknown <jtc7537@gmail.com>
2018-05-14 22:15:35 -0400

    geoloc

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+const axios = require("axios"); 
 
 const GoogleMapKEY = "&key=AIzaSyD0CbzacbBZXUg0C2cftLGr-p4GHFP0cAc";
 const ParkWhizKEY = "&api_key=4206ee6642163fb508fb9b94ba7b04481e07ddf8";
@@ -7,6 +7,10 @@ const ParkWhizKEY = "&api_key=4206ee6642163fb508fb9b94ba7b04481e07ddf8";
 const GoogleMapURL = "https://maps.googleapis.com/maps/api/geocode/json?address=";
 const ParkWhizURL = "https://api.parkwhiz.com/v4/quotes/?q=coordinates:";
 
+const today = new Date()
+
+console.log(today)
+
 export default {
     searchMap: function(address){
         console.log(GoogleMapURL + address + GoogleMapKEY);
@@ -14,7 +18,7 @@ export default {
     },
 
     searchParking: function(lat, lng){
-        console.log(ParkWhizURL + lat + lng + "&start_time=2018-05-14T12:00&end_time=2018-05-14T20:00" + ParkWhizKEY);
+        console.log(ParkWhizURL + lat + lng + `&start_time=${today}&end_time=${today + 1}` + ParkWhizKEY);
         return axios.get(ParkWhizURL + lat +","+ lng + "&start_time=2018-05-14T12:00&end_time=2018-05-14T20:00" + ParkWhizKEY);
     }
 }
