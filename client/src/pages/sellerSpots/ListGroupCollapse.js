7a56ef63fc1dec770373eb03196d186745aab23b
Bettina Junghahn <bettinajunghahn@gmail.com>
2018-05-15 14:15:23 -0400

    responsiveness SellerSpots page and elements

--- client/src/pages/sellerSpots/ListGroupCollapse.js
+++ client/src/pages/sellerSpots/ListGroupCollapse.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardImg, CardText,
+import { Col, Container, Row, Card, CardImg, CardText,
     CardTitle, CardSubtitle, Button, Collapse } from 'reactstrap';
 
 import './ListGroupCollapse.css';
@@ -20,26 +20,32 @@ class ListGroupCollapse extends React.Component {
     const cat = this.props.cat;
     return (
         <div>
-            <Card className="border">
-                <div>
-                <CardTitle>
-                    <strong>{cat.name}</strong>
-                </CardTitle>
-                <CardSubtitle>
-                    {cat.spotType}  
-                </CardSubtitle>
-                <CardText className="text-left">
-                        {cat.address} <br/>
-                        Price per Hour: {cat.price} <br/>
-                        Spot Description: {cat.spotDescription}
-                </CardText> 
-                <CardImg className="imgToggle" top width="100%" src={cat.spotImg} alt="Parking Image"/> 
-                <br/>
-                <Button size="sm" color="info" id="deleteRental">Edit</Button>{" "}
-                <Button size="sm" color="info" id="deleteRental">Delete </Button>
-                    <br/>
-                </div>
-            </Card>
+            <Container fluid className="full-height" id="card-block">
+                <Row className="h-100 justify-content-center full-height align-items-center">
+                    <Col  className="p-0">
+                        <Card className="border">
+                            <div>
+                            <CardTitle>
+                                <strong>{cat.name}</strong>
+                            </CardTitle>
+                            <CardSubtitle>
+                                {cat.spotType}  
+                            </CardSubtitle>
+                            <CardText className="text-left">
+                                    {cat.address} <br/>
+                                    Price per Hour: {cat.price} <br/>
+                                    Spot Description: {cat.spotDescription}
+                            </CardText> 
+                            <CardImg className="imgToggle" top width="100%" src={cat.spotImg} alt="Parking Image"/> 
+                            <br/>
+                            <Button size="sm" color="info" id="deleteRental">Edit</Button>{" "}
+                            <Button size="sm" color="info" id="deleteRental">Delete </Button>
+                                <br/>
+                            </div>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>             
             <br/>
         </div>
     );
