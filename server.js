260ce4c0190df70061f02bae2f40b476fbdb159b
unknown <jtc7537@gmail.com>
2018-05-14 20:02:26 -0400

    build

--- server.js
+++ server.js
@@ -31,9 +31,9 @@ app.use('/api/auth', auth);
 app.use('/spaces', spaces)
 
 
-// app.get('*', (req, res) => {
-// 	res.sendFile(path.join(__dirname, './client/build/index.html'))
-// })
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, './client/build/index.html'))
+})
 
 
 // catch 404 and forward to error handler
