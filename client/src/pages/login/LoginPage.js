97842950650d467693147cb472a4eca6dbd901e3
unknown <jtc7537@gmail.com>
2018-05-22 20:10:31 -0400

    styling

--- client/src/pages/login/LoginPage.js
+++ client/src/pages/login/LoginPage.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
@@ -77,13 +78,19 @@ class Login extends React.Component {
 
         const { username, password, message } = this.state;
 
+        const imgStyle = {width:'350px', marginLeft:'auto', marginRight:'auto', marginTop:'50px', boxShadow:'none'}
+
+        const bottomButton = {backgroundColor:'#3D3D9D', color:'white',width:'100%',border:'none',height:'60px',fontWeight:'bold'}
+        const bottomButtonOrange = {backgroundColor:'#FF9900', color:'white',width:'100%',border:'none',height:'60px',fontWeight:'bold'}
+        const title = {color:'#3D3D9D',fontSize:'40px',textAlign:'center',width:'100%',fontWeight:'600'}
         return(
             <div className="container" id="full-body">
-                <Background backgroundImage="https://image.freepik.com/free-vector/city-background-design_1300-365.jpg">
-                    <Title/>
+                <Background backgroundImage="">
+                    <div style={title}>LANDING PAD</div>
                     <p className="title-description">A parking rental app</p>
+                    <img src='https://i.imgur.com/HKH2zed.gif' style={imgStyle} />
                     <div className=" fixed-bottom" id="sign-in">
-                        <Button outline color="primary" className="simple-btn" onClick={this.showModal.bind(this, "modal1")}>Sign In</Button>
+                        <button color="primary" style={bottomButton} onClick={this.showModal.bind(this, "modal1")}>SIGN IN</button>
                         <Modal isOpen={this.state.modal1} toggle={this.closeModal.bind(this, "modal1")} className={this.props.className} id="enter-modal">
                         <ModalHeader toggle={this.closeModal.bind(this, "modal1")} className="mod-title">Sign In</ModalHeader>
                             <ModalBody className="mod-body">
@@ -125,11 +132,11 @@ class Login extends React.Component {
                                     AUTH
                         </button> */}
                     </div>
-                    <div className=" fixed-bottom" id="sign-up">
-                        <Link to="/signup"><Button outline color="primary" className="simple-btn" onClick={this.showModal.bind(this, "modal2")}>Sign Up</Button></Link>
+                    <div className="fixed-bottom" id="sign-up">
+                        <Link to="/signup"><button outline color="primary" style={bottomButtonOrange} onClick={this.showModal.bind(this, "modal2")}>SIGN UP</button></Link>
                     </div>
 
-                    <Footer/>
+                    
                 </Background>
             </div>
         )
@@ -137,3 +144,58 @@ class Login extends React.Component {
 }
 
 export default Login;
+
+const oStyle = `<div className="container" id="full-body">
+<Background backgroundImage="https://image.freepik.com/free-vector/city-background-design_1300-365.jpg">
+    <Title/>
+    <p className="title-description">A parking rental app</p>
+    <div className=" fixed-bottom" id="sign-in">
+        <Button outline color="primary" className="simple-btn" onClick={this.showModal.bind(this, "modal1")}>Sign In</Button>
+        <Modal isOpen={this.state.modal1} toggle={this.closeModal.bind(this, "modal1")} className={this.props.className} id="enter-modal">
+        <ModalHeader toggle={this.closeModal.bind(this, "modal1")} className="mod-title">Sign In</ModalHeader>
+            <ModalBody className="mod-body">
+                <FormGroup row>
+                    <Label for="exampleUsername" sm={2}>Username</Label>
+                    <Col sm={10}>
+                        <Input 
+                        type="username" 
+                        name="username" 
+                        id="username" 
+                        placeholder="username"
+                        value={username} 
+                        onChange={this.onChange} required/>
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                    <Label for="examplePassword" sm={2}>Password</Label>
+                    <Col sm={10}>
+                        <Input 
+                        type="password" 
+                        name="password" 
+                        id="examplePassword" 
+                        placeholder="password"
+                        value={password} 
+                        onChange={this.onChange} required/>
+                    </Col>
+                </FormGroup>
+            </ModalBody>
+            <ModalFooter className="mod-foot">
+                <Button color="primary" onClick={this.onSubmit}>Submit</Button>
+                    {message !== '' &&
+                        <div className="alert alert-warning alert-dismissible" role="alert">
+                        { message }
+                        </div>
+                    }
+            </ModalFooter>
+        </Modal>
+        {/* <button onClick={() => this.authStorage()}>
+                    AUTH
+        </button> */}
+    </div>
+    <div className=" fixed-bottom" id="sign-up">
+        <Link to="/signup"><Button outline color="primary" className="simple-btn" onClick={this.showModal.bind(this, "modal2")}>Sign Up</Button></Link>
+    </div>
+
+    <Footer/>
+</Background>
+</div>`
